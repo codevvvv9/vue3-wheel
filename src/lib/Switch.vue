5f5d6c3441fa6bb61d16590059768ca12d8bffc9
wushao <15265249516@163.com>
2022-09-21 21:34:49 +0800

    What: 06-switch组件内切换
    Why:
    
    *
    
    How:
    
    *

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
-    <button>
+    <button :class="{checked: checked}" @click="toggleSwitch">
       <span></span>
     </button>
   </div>
 </template>
 <script lang="ts">
+import { Ref, ref } from 'vue';
+
 export default {
-  name: 'Switch'
+  name: 'Switch',
+  setup(props: any, context: { commit: (arg0: string, arg1: Ref<boolean>) => void; }) {
+    const checked = ref(false)
+    const toggleSwitch = () => {
+      checked.value = !checked.value
+    }
+    // context.commit('update:value', checked)
+    return {
+      checked,
+      toggleSwitch,
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
@@ -17,7 +30,10 @@ export default {
     height: $h;
     width: calc($h * 2);
     border: none;
-    background: blue;
+    &:focus {
+      outline: none;
+    }
+    background: gray;
     border-radius: calc($h / 2);
     position: relative;
     span {
@@ -28,10 +44,16 @@ export default {
       width: $h2;
       border-radius: calc($h2 / 2);
       background: white;
+      transition: left 300ms;
     }
-    &:hover > span {
-      left: calc(100% - #{$h2} - 2px);
+    &:hover {
       cursor: pointer;
+    }
+    &.checked {
+      background: blue;
+    }
+    &.checked > span {
+      left: calc(100% - #{$h2} - 2px);
     }
   }
 </style>
