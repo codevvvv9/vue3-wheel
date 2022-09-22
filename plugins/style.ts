965d1c4a370a443da10bb990290a4b60726bda51
wushao <15265249516@163.com>
2022-09-22 22:45:13 +0800

    What:  16-修复vite3引入md并解析md的问题，修改vite3插件
    Why:
    
    *
    
    How:
    
    *

--- plugins/style.ts
+++
@@ -1,5 +0,0 @@
-export const addStyle = (style: string): void => {
-  const styleElement = document.createElement('style');
-  styleElement.innerHTML = style;
-  document.getElementsByTagName('head')[0].appendChild(styleElement);
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import markdown from './plugins/index'
-import { plugin, Mode } from 'vite-plugin-markdown'
+// import { plugin, Mode } from 'vite-plugin-markdown'
+import { md } from './plugins/md-vite3'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     // markdown(),
-    plugin({mode: [Mode.HTML, Mode.VUE]}),
+    // plugin({mode: [Mode.HTML, Mode.VUE]}),
+    md()
   ],
   base: './'
 })
